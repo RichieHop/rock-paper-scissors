9007c551fc2dc48c9bf12a841668707f3cf68e33
Richie <richiehopkinson@hotmail.co.uk>
2025-06-30 16:10:26 +0100

    Update rock, paper, scissors game with a UI.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,25 +1,11 @@
-
-let computerResults = "";
-let humanResults = "";
 let computerScore = 0;
 let humanScore = 0;
-let myResults = "";
-
-function getComputerChoice() {
-  let computerChoice = getRandomIntInclusive(1, 3);
-  return computerChoice;
-}
-
-function getHumanChoice() {
-  let humanChoice = prompt("Enter either Rock, Paper or Scissors");
-  return humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
-}
+let currentRound = 0;
 
 function getRandomIntInclusive(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
-  let randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-  let computerChoice = "";
+  let randomNumber = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum and the minimum are inclusive
 
   if (randomNumber === 1) {
     return "Rock";
@@ -29,33 +15,38 @@ function getRandomIntInclusive(min, max) {
         return "Paper";}
 }
 
+function getComputerChoice() {
+  let computerChoice = getRandomIntInclusive(1, 3);
+  return computerChoice;
+}
   function getResultAndScore(computerChoice, humanChoice) {
+
     let humanScore =0;
     let computerScore = 0;
     let result = "";
 
     if (computerChoice == "Rock" && humanChoice == "Rock") {
-      result = "Draw - you both chose rock"; computerScore = 0; humanScore = 0
+      result = "Draw - you both chose rock"; computerScore = 0; humanScore = 0;
     } else if (computerChoice == "Rock" && humanChoice == "Scissors") {
-      result = "Computer wins! Rock blunts scissors"; computerScore = 1; humanScore = 0
+      result = "Computer wins! Rock blunts scissors"; computerScore = 1; humanScore = 0;
       } else if (computerChoice == "Rock" && humanChoice == "Paper") {
-        result = "You win! Paper wraps rock"; computerScore = 0; humanScore = 1
+        result = "You win! Paper wraps rock"; computerScore = 0; humanScore = 1;
         } 
 
     if (computerChoice == "Paper" && humanChoice == "Paper") {
-      result = "Draw - you both chose paper"; computerScore = 0; humanScore = 0
+      result = "Draw - you both chose paper"; computerScore = 0; humanScore = 0;
     } else if (computerChoice == "Paper" && humanChoice == "Rock") {
-      result = "Computer wins! Paper wraps rock"; computerScore = 1; humanScore = 0
+      result = "Computer wins! Paper wraps rock"; computerScore = 1; humanScore = 0;
       } else if (computerChoice == "Paper" && humanChoice == "Scissors") {
-        result = "You win! Scissors cut paper"; computerScore = 0; humanScore = 1
+        result = "You win! Scissors cut paper"; computerScore = 0; humanScore = 1;
         } 
 
     if (computerChoice == "Scissors" && humanChoice == "Scissors") {
-      result = "Draw - you both chose scissors"; computerScore = 0; humanScore = 0
+      result = "Draw - you both chose scissors"; computerScore = 0; humanScore = 0;
     } else if (computerChoice == "Scissors" && humanChoice == "Paper") {
-      result = "Computer wins! Scissors cut paper"; computerScore = 1; humanScore = 0
+      result = "Computer wins! Scissors cut paper"; computerScore = 1; humanScore = 0;
       } else if (computerChoice == "Scissors" && humanChoice == "Rock") {
-        result = "You win! Rock blunts scissors"; computerScore = 0; humanScore = 1
+        result = "You win! Rock blunts scissors"; computerScore = 0; humanScore = 1;
         } 
 
     return {
@@ -65,19 +56,66 @@ function getRandomIntInclusive(min, max) {
     }
   }
 
-for (let loop = 1; loop < 6; loop++) {;
-  let computerChoice = (getComputerChoice());
-  let humanChoice = (getHumanChoice());
+const choicesDetail = document.querySelector(".choicesDetail");
+const innerResultsDetail = document.querySelector(".innerResultsDetail");
+const scoresDetail = document.querySelector(".scoresDetail");
+const roundDetail = document.querySelector(".roundDetail");
 
-  let result = (getResultAndScore(computerChoice, humanChoice));
-  computerScore = computerScore + result.computerScore;
-  humanScore = humanScore + result.humanScore;
+// Get the human selection.
+const buttons = document.querySelectorAll("button");
+
+// Use the .forEach method to iterate through each button....
+buttons.forEach((button) => {
+  // .... and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    currentRound == ++currentRound;
+    const computerChoice = (getComputerChoice());
+    const humanChoice = button.id;
+    const result = (getResultAndScore(computerChoice, humanChoice));
+    computerScore = computerScore + result.computerScore;
+    humanScore = humanScore + result.humanScore;
+
+    // Display the round number, result and score in the resultsDetail div.
+    roundDetail.textContent = "Round " + currentRound;
+    choicesDetail.textContent = "Computer choice is " + computerChoice + ", your choice is " + humanChoice;
+    innerResultsDetail.textContent = "Result: "  + result.result; 
+    scoresDetail.textContent = "Scores: " + computerScore + " - " + humanScore;
+
+    // Check if either the computer or human has reached 5 points; if so then end the game.
+    if (computerScore == 5) {
+      // Get the modal
+      var modal = document.getElementById("myModal");
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+      document.getElementById('modalText').innerText = "Game over, computer wins. Close this window to start a new game.";
+      modal.style.display = "block";
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = "none";
+        computerScore = 0; humanScore = 0; currentRound=0;
+        roundDetail.textContent = "";
+        choicesDetail.textContent = "";
+        innerResultsDetail.textContent = ""; 
+        scoresDetail.textContent = "";  
+      }
+    
+    } else if (humanScore == 5) {
+      // Get the modal
+      var modal = document.getElementById("myModal");
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+      document.getElementById('modalText').innerText = "Game over, you win! Close this window to start a new game.";
+      modal.style.display = "block";
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = "none";
+        computerScore = 0; humanScore = 0; currentRound=0;
+        roundDetail.textContent = "";
+        choicesDetail.textContent = "";
+        innerResultsDetail.textContent = ""; 
+        scoresDetail.textContent = ""; 
+      }
+    }
   
-  computerResults = computerResults + computerChoice + "\n"; 
-  humanResults = humanResults + humanChoice + "\n"; 
-  myResults = myResults + result.result + "\n"; 
-
-}
-
-alert ("Computer choices are: " + computerResults + "\nYour choices are: " + humanResults);
-alert ("Results are: "  + "\n" + myResults + "\n" + "Scores are: " + computerScore + " - " + humanScore);
+  });
+});
